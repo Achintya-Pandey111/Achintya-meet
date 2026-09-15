60a3f8b7736b17e6087b51de9089e2a7b7822af3
Achintya-Pandey111 <achintya2316pandey@gmail.com>
2026-09-16 00:44:21 +0530

    feat: updated id's of fake events

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,25 @@ const express = require('express');
 
 const app = express();
 
+const events = [
+    {
+        id: '101',
+        title: 'Tekron',
+        description: 'This is tekron event'
+    },
+    {
+        id:'201',
+        title: 'blood donation camp',
+        description: 'this is a blood donation camp'
+    }
+];
+
+app.get('/events',(req,res)=>{
+    res.json(events)
+})
+
+
+
 app.listen(3000,()=>{
     console.log('server started on port 3000')
 })
